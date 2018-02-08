4d2667ac6b07dab38a3ef6d1cb6efae0dd0a4e88
Jackson <jhdocker@gmail.com>
2018-02-08 11:26:01 -0500

    reverting out a3340f646c65ae681e240527535164c5e3d85032

--- index.php
+++ index.php
@@ -7,6 +7,9 @@ $sGreeting = 'Devops Test';
 <html>
     <h1><?echo $sGreeting;?></h1>
     <p>testing server edit</p>
+<<<<<<< HEAD
     <p>added third line</p>
     <p>added fourth line</p>
+=======
+>>>>>>> parent of a3340f6... adding new <p>. going to test revert of
 </html>
