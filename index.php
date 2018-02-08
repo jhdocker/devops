ce7a583115eebc3cf9c9a3d1f48906941c1611a9
Jackson <jhdocker@gmail.com>
2018-02-08 11:28:32 -0500

    removing git revert conflicts and starting from scratch

--- index.php
+++ index.php
@@ -7,9 +7,4 @@ $sGreeting = 'Devops Test';
 <html>
     <h1><?echo $sGreeting;?></h1>
     <p>testing server edit</p>
-<<<<<<< HEAD
-    <p>added third line</p>
-    <p>added fourth line</p>
-=======
->>>>>>> parent of a3340f6... adding new <p>. going to test revert of
 </html>
