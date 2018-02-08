aa27203241c3b362b579437afb03e4222b3fed93
Jackson <jhdocker@gmail.com>
2018-02-08 11:06:18 -0500

    removing second <p> tag to test

--- index.php
+++ index.php
@@ -7,5 +7,4 @@ $sGreeting = 'Devops Test';
 <html>
     <h1><?echo $sGreeting;?></h1>
     <p>testing server edit</p>
-    <p>third edit. going to revert second push with 'testing server edit'</p>
 </html>
