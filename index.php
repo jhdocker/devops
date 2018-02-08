a3340f646c65ae681e240527535164c5e3d85032
Jackson <jhdocker@gmail.com>
2018-02-08 10:17:09 -0500

    adding new <p>. going to test revert of commit 3d40f18fc4921cd199951d85d34d8c49d7878830

--- index.php
+++ index.php
@@ -7,4 +7,5 @@ $sGreeting = 'Devops Test';
 <html>
     <h1><?echo $sGreeting;?></h1>
     <p>testing server edit</p>
+    <p>third edit. going to revert second push with 'testing server edit'</p>
 </html>
